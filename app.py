fdeeb4cba4f02934f2bca7b4274eb67cdd1115a4
Yiqiao Yin <eagle0504@gmail.com>
2023-07-27 01:05:49 -0400

    app updated

--- app.py
+++ app.py
@@ -9,22 +9,18 @@ st.title("ChatGPT-like clone")
 # Set the OpenAI API key from Streamlit secrets
 openai.api_key = st.secrets["OPENAI_API_KEY"]
 
-
 # Check if "openai_model" is not stored in session state and set it to "gpt-4"
 if "openai_model" not in st.session_state:
     st.session_state["openai_model"] = "gpt-4"
-
 
 # Check if "messages" is not stored in session state and set it to an empty list
 if "messages" not in st.session_state:
     st.session_state.messages = []
 
-
 # Iterate over each message in the session state messages
 for message in st.session_state.messages:
     with st.chat_message(message["role"]):
         st.markdown(message["content"])
-
 
 # Get user input from chat_input and store it in the prompt variable using the walrus operator ":="
 if prompt := st.chat_input("What is up?"):
